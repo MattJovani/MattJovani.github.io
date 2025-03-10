a943c7daecb5cd2df54beb5211bf4ebb663d367c
Matthew Jovani <109990454+MattJovani@users.noreply.github.com>
2025-03-09 23:19:24 -0400

    Create Horror.js

---
+++ AgentPhoenix/Tapple/Horror.js
@@ -0,0 +1,80 @@
+const horrorTapple = [
+    {letter: "A", items: [
+        "Annabelle"
+    ]},
+    {letter: "B", items: [
+       
+    ]},
+    {letter: "C", items: [
+        
+    ]},
+    {letter: "D", items: [
+        
+    ]},
+    {letter: "E", items: [
+       
+    ]},
+    {letter: "F", items: [
+        
+    ]},
+    {letter: "G", items: [
+        
+    ]},
+    {letter: "H", items: [
+        
+    ]},
+    {letter: "I", items: [
+        
+    ]},
+    {letter: "J", items: [
+        
+    ]},
+    {letter: "K", items: [
+        
+    ]},
+    {letter: "L", items: [
+       
+    ]},
+    {letter: "M", items: [
+        
+    ]},
+    {letter: "N", items: [
+        
+    ]},
+    {letter: "O", items: [
+        
+    ]},
+    {letter: "P", items: [
+       
+    ]},
+    {letter: "Q", items: [
+        
+    ]},
+    {letter: "R", items: [
+       
+    ]},
+    {letter: "S", items: [
+        
+    ]},
+    {letter: "T", items: [
+        
+    ]},
+    {letter: "U", items: [
+        
+    ]},
+    {letter: "V", items: [
+        
+    ]},
+    {letter: "W", items: [
+        
+    ]},
+    {letter: "X", items: [
+        
+    ]},
+    {letter: "Y", items: [
+        
+    ]},
+    {letter: "Z", items: [
+        
+    ]},
+]
