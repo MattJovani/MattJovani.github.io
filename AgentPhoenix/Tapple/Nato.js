d65f6fe72715d918a7f1aaacfc5504ad61bcdd31
Matthew Jovani <109990454+MattJovani@users.noreply.github.com>
2025-03-18 08:46:07 -0400

    Created Nato.js

---
+++ AgentPhoenix/Tapple/Nato.js
@@ -0,0 +1,80 @@
+const natoTapple = [
+    {letter: "A", items: [
+        "Alpha"
+    ]},
+    {letter: "B", items: [
+       "Bravo"
+    ]},
+    {letter: "C", items: [
+        "Charlie"
+    ]},
+    {letter: "D", items: [
+        "Delta"
+    ]},
+    {letter: "E", items: [
+       "Echo"
+    ]},
+    {letter: "F", items: [
+        "Foxtrot"
+    ]},
+    {letter: "G", items: [
+        "Golf"
+    ]},
+    {letter: "H", items: [
+        "Hotel"
+    ]},
+    {letter: "I", items: [
+        "India"
+    ]},
+    {letter: "J", items: [
+        "Juliette"
+    ]},
+    {letter: "K", items: [
+        "Kilo"
+    ]},
+    {letter: "L", items: [
+       "Lima"
+    ]},
+    {letter: "M", items: [
+        "Mike"
+    ]},
+    {letter: "N", items: [
+        "November"
+    ]},
+    {letter: "O", items: [
+        "Oscar"
+    ]},
+    {letter: "P", items: [
+       "Papa"
+    ]},
+    {letter: "Q", items: [
+        "Quebec"
+    ]},
+    {letter: "R", items: [
+       "Romeo"
+    ]},
+    {letter: "S", items: [
+        "Sierra"
+    ]},
+    {letter: "T", items: [
+        "Tango"
+    ]},
+    {letter: "U", items: [
+        "Uniform"
+    ]},
+    {letter: "V", items: [
+        "Victor"
+    ]},
+    {letter: "W", items: [
+        "Whiskey"
+    ]},
+    {letter: "X", items: [
+        "Xray"
+    ]},
+    {letter: "Y", items: [
+        "Yankee"
+    ]},
+    {letter: "Z", items: [
+        "Zulu"
+    ]},
+]
